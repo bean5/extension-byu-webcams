f308e5568d478c8c4257f21ace9774b02f580a9d
bean5 <2052646+bean5@users.noreply.github.com>
2015-06-02 07:09:31 -0600

    initial dynamic load version; no blacklisting exists for known 'bad' camera paths

--- javascript.js
+++ javascript.js
@@ -1,99 +1,123 @@
-setInterval(updateKimballTower, 60 * 1000);
-// setInterval(updateConstruction, 400);//no known construction webcams at this time
-setInterval(updateBroadcasting, 500);
-setInterval(updateTestingCenter, 7 * 1000);
-setInterval(updateCardCenter, 1*1000);
-setInterval(updateBrimhall, 60*1000);
-setInterval(updateStudentFitness, 60*1000);
+$(function() {
+	var defaultValues = 
+	[
+	    {
+	        "id": "testing",
+	        "name": "Testing Center",
+	        "description": "Testing Center Line Conditions",
+	        "refreshInterval": 15,
+	        "location": "https://ws.byu.edu/services/cameras/rest/v1/streams/testing"
+	    },
+	    {
+	        "id": "id-center",
+	        "name": "ID Center",
+	        "description": "View of the ID Center in the WSC",
+	        "refreshInterval": 15,
+	        "location": "https://ws.byu.edu/services/cameras/rest/v1/streams/id-center"
+	    },
+	    {
+	        "id": "timp-esc",
+	        "name": "Mt. Timpanogos (from ESC)",
+	        "description": "",
+	        "refreshInterval": 5,
+	        "location": "https://ws.byu.edu/services/cameras/rest/v1/streams/timp-esc"
+	    },
+	    {
+	        "id": "traverse-swkt",
+	        "name": "Traverse Ridge (from SWKT)",
+	        "description": "",
+	        "refreshInterval": 60,
+	        "location": "https://ws.byu.edu/services/cameras/rest/v1/streams/traverse-swkt"
+	    },
+	    {
+	        "id": "daily-universe",
+	        "name": "Daily Universe Camera",
+	        "description": "",
+	        "refreshInterval": 60,
+	        "location": "https://ws.byu.edu/services/cameras/rest/v1/streams/daily-universe"
+	    },
+	    {
+	        "id": "brigham-square",
+	        "name": "Brigham Square",
+	        "description": "",
+	        "refreshInterval": 2,
+	        "location": "https://ws.byu.edu/services/cameras/rest/v1/streams/brigham-square"
+	    },
+	    {
+	        "id": "bookstore1",
+	        "name": "Bookstore Camera 1",
+	        "description": "Bookstore Cam",
+	        "refreshInterval": 2,
+	        "location": "https://ws.byu.edu/services/cameras/rest/v1/streams/bookstore1"
+	    },
+	    {
+	        "id": "bookstore2",
+	        "name": "Bookstore Camera 2",
+	        "description": "Bookstore Cam",
+	        "refreshInterval": 2,
+	        "location": "https://ws.byu.edu/services/cameras/rest/v1/streams/bookstore2"
+	    },
+	    {
+	        "id": "bookstore-quad",
+	        "name": "All four Bookstore feeds",
+	        "description": "All four bookstore feeds.",
+	        "refreshInterval": 2,
+	        "location": "https://ws.byu.edu/services/cameras/rest/v1/streams/bookstore-quad"
+	    },
+	    {
+	        "id": "broadcasting-nw",
+	        "name": "Broadcasting Building (NW)",
+	        "description": "",
+	        "refreshInterval": 5,
+	        "location": "https://ws.byu.edu/services/cameras/rest/v1/streams/broadcasting-nw"
+	    },
+	    {
+	        "id": "broadcasting-sw",
+	        "name": "Broadcasting Building (SW)",
+	        "description": "",
+	        "refreshInterval": 5,
+	        "location": "https://ws.byu.edu/services/cameras/rest/v1/streams/broadcasting-sw"
+	    },
+	    {
+	        "id": "student-fitness",
+	        "name": "Student Fitness Center",
+	        "description": "See how busy the fitness center is",
+	        "refreshInterval": 5,
+	        "location": "https://ws.byu.edu/services/cameras/rest/v1/streams/student-fitness"
+	    }
+	];
 
-//For future use: useLocal = typeof(Storage)!=='undefined';
+	var useService = true;//can be disabled if one doesn't want to use the service.
 
-//Probably now defunct along with getUpdatedSource()
-//if(localStorage.timp_src == null || localStorage.timp_src == '')
-	// previously used http://marvin.byu.edu/Weather/tmp/w16361-134532371.jpeg
-	//localStorage.timp_src = 'http://marvin.byu.edu/Weather/webcam800.jpg';
+	if(useService) 
+	{
+		//TODO: catch failures in case service goes down.
+		$.get('https://soaregistry.byu.edu/services/campusInformation/webcam/v1/streams/', function(response) {
+			loadValues(response);
+		});
+	}
+	else 
+	{
+		loadValues(defaultValues);
+	}
 
-// getUpdatedSource(); //update source of marvin src
-//setInterval(getUpdatedSource, 10 * 1 * 1000);
-setInterval(updateMtTimp, 1000);
+	function loadValues(values) {
+		//add the element to index.html
+		//set interval to match interval named in html
+		//loop through each key
+		$.each(values, function(key, value) {
+			$('body').append('<h1>'+value.name+'</h1>');
+			$('body').append('<h2>'+value.description+'</h2>');
+			$('body').append('<img id="'+value.id+'" src="'+value.location+'" style="width:440px;" >');
 
-function getUpdatedSource()
-{
-	//deprecated/defunct, return early if called
-	return;
-	$.ajax({
-		type: 'GET',
-		url: 'http://students.cs.byu.edu/~bean5/webcam_helpers/marvin_timp.php',
-		success:function(data){
-			localStorage.timp_src = data;
-			updateMtTimp();
-		}
-	}).done(function() {
-		console.log('source updated');
-	});
-}
+			//allow for refreshing of images automatically (seconds, depending on interval provided)
+			setInterval(function() {
+				updateImageWith(value.id, value.location);
+			}, value.refreshInterval * 1000);
+		});
+	}
 
-function updateImageWith(imgID, src, useTimeStampString) {
-	$('#' + imgID).attr('src', src + (useTimeStampString ? '?timestamp=' + Date() : ''));
-}
-
-/*
-	Main image update functions.
-	TODO: since most of these are so similar, consider just using anonymous functions
-*/
-function updateKimballTower()
-{
-	updateImageWith('kimballTower', 'http://psych.byu.edu/files/Picture1.jpg', true);
-}
-
-function updateMtTimp()
-{
-	if(localStorage.timp_src == '')
-		$('#timpArea').hide();
-	else
-		$('#timpArea').show();
-
-	updateImageWith('mtTimp', 'http://marvin.byu.edu/Weather/webcam800.jpg', true);
-}
-
-function updateBroadcasting()
-{
-	/*previously used:
-		http://byubwc.byu.edu/building/live/readImage.asp
-		http://byubwc.byu.edu/building/live/readImage2.asp
-	*/
-	updateImageWith('broadcasting', 'https://ws.byu.edu/services/cameras/rest/v1/streams/broadcasting-nw', true);
-}
-
-function updateConstruction()
-{
-	return;//No known construction cameras at this time.
-	updateImageWith('construction1', 'http://lswebcam1.byu.edu/cam/cam_1.jpg', true);
-	updateImageWith('construction2', 'http://lswebcam1.byu.edu/cam/bnsn/bnsn1.jpg', true);
-}
-
-function updateTestingCenter()
-{
-	/*
-		previously used:
-		http://testing.byu.edu/images/conditions/axiscam.jpg
-		https://testing.byu.edu/sites/all/scripts/linecamfeed.php
-	*/
-	updateImageWith('testing_center', 'https://ws.byu.edu/services/cameras/rest/v1/streams/testing', true);
-}
-
-function updateStudentFitness() {
-	updateImageWith('fitness', 'https://ws.byu.edu/services/cameras/rest/v1/streams/student-fitness', true)
-}
-
-function updateCardCenter()
-{
-	//Previously used http://10.25.116.231/axis-cgi/jpg/image.cgi?resolution=640x480, which only works from on campus...
-	updateImageWith('card_center', 'https://ws.byu.edu/services/cameras/rest/v1/streams/id-center', true);
-}
-
-function updateBrimhall()
-{
-	// Previously used: http://newsline.byu.edu/webcam/brmb-e/brmb-e.jpg
-	updateImageWith('brim', 'https://ws.byu.edu/services/cameras/rest/v1/streams/daily-universe', true);
-}
+	function updateImageWith(imgID, src, useTimeStampString) {
+		$('#' + imgID).attr('src', src + (useTimeStampString ? '?timestamp=' + Date() : ''));
+	}
+});
